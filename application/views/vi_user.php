16d449f8ade3cf7bbfd895102ec3258bb1b75044
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-30 11:50:48 +0700

    card

--- application/views/vi_user.php
+++ application/views/vi_user.php
@@ -1,8 +1,8 @@
 <!-- CHART GRAFIK USER-->
-        <section>
+        <section class="card-responsive">
             <div class="card-body mx-12 px-5 my-3">
 		        <?php foreach ($countkasus as $value) {?>
-        	    <div class= "stretch-card">
+        	    <div class= "stretch-card responsive">
                         <div class="col-lg-2 mx-4 px-2 py-1 grid-margin stretch-card">
 							<div class="card" style="background: rgba(0, 99, 132, 0.1)">
 								<div class="card-body">
