f6d9bff5919f8c220f302dbecdc224475c3e2b7f
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-30 11:17:29 +0700

    sig

--- application/views/layout2/vi_nav.php
+++ application/views/layout2/vi_nav.php
@@ -1,7 +1,7 @@
        <!-- Header - set the background image for the header in the line below-->
-       <header class="py-5 bg-image-full" style="background-image: url('<?= base_url() ?>assets/stepan-unsplash.jpg'); opacity:0.9;">
+       <header class="py-5 bg-image-full" style="background-image: url('<?= base_url() ?>assets/stepan-unsplash.jpg'); opacity:0.8;">
             <div class="text-center my-5" >
-                <h1 class="text-white fs-3 fw-bolder">SISTEM INFORMASI GEOGRAFIS</h1>
+                <h1 class="text-white-50 mb-0 fs-3 fw-bolder">SISTEM INFORMASI GEOGRAFIS</h1>
                 <h3 class="text-white fs-3 fw-bolder">PEMETAAN TITIK LOKASI DAERAH RAWAN KECELAKAAN LALU LINTAS</h3>
                 <h4 class="text-white-50 mb-0 fw-bolder">Kabupaten Bantul, Daerah Istimewa Yogyakarta</h4>
             </div>
