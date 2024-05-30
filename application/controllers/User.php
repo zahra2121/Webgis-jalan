9aa7d86184b36d81cb52e1237e306b636b9d7294
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-30 10:30:48 +0700

    lagi 9

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,8 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User extends CI_Controller {
 	
 	function __construct(){
-		//parent::__construct();
-        //$this->CI =& get_instance();
+		parent::__construct();
+        $this->CI =& get_instance();
         $this->load->model('M_user');
 	}
  
