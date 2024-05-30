53b3d84e6665412ebbe3e50bde3d1ebf98405af2
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-30 14:10:09 +0700

    detail25

--- application/views/vi_user.php
+++ application/views/vi_user.php
@@ -110,6 +110,7 @@
 
 
                         var peta5 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                            maxZoom: 18,
                             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         });
 
