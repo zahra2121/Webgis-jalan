db04d177bdb01a40ae0b88a33fe1b43ac42756f4
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-30 10:42:29 +0700

    lagi 11

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,8 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends CI_Controller {
 	
 	public function __construct(){
-        parent::__construct();  
-        $this->load->library('session');
+        //parent::__construct();  
+        //$this->load->library('session');
         $this->load->library(array('form_validation'));
       	$this->load->helper(array('url','form'));
        	$this->load->model('M_login'); //call model	
