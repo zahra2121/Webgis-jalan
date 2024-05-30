7a4f4ca289d19e3a7fe2d67b5eb708572ac4394c
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-30 10:49:10 +0700

    lagu13

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Register extends CI_Controller {
 	
 	function __construct(){
-		parent::__construct();
+		//parent::__construct();
 		$this->load->library(array('form_validation'));
       	$this->load->helper(array('url','form'));
        	$this->load->model('m_login'); //call model	
