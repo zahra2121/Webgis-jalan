94a3ad2c165fb28f9a4f4bd7e71ea3a0d33a5e28
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-30 10:57:16 +0700

    parent ganti

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends CI_Controller {
 	
 	public function __construct(){
-        //parent::__construct();  
+        parent::__construct();  
         $this->load->library('session');
         $this->load->library(array('form_validation'));
       	$this->load->helper(array('url','form'));
