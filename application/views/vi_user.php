cb95184facc30ab59237a0c900ec7be492d18cfd
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-30 12:10:34 +0700

    card12

--- application/views/vi_user.php
+++ application/views/vi_user.php
@@ -2,8 +2,8 @@
         <section class="card-responsive">
             <div class="card-body mx-12 px-5 my-3">
 		        <?php foreach ($countkasus as $value) {?>
-        	    <div class= "stretch-card">
-                        <div class="col-lg-2 mx-4 px-2 py-1 card-responsive">
+        	    <div class= "responsive">
+                        <div class="col-lg-2 mx-4 px-2 py-1 grid-margin 50% stretch-card">
 							<div class="card" style="background: rgba(0, 99, 132, 0.1)">
 								<div class="card-body">
                                     <h3 class="card-title mb-1 text-dark">DATA KASUS</h3>
@@ -15,7 +15,7 @@
 						</div>
                         <?php }?>
                         <?php foreach ($countblack as $key => $value) {?>
-                            <div class="col-lg-2 px-2 mx-4 py-1 card-responsive">
+                            <div class="col-lg-2 px-2 mx-4 py-1 grid-margin 50% stretch-card">
 							<div class="card" style="background: rgba(0, 99, 132, 0.1)">
 								<div class="card-body">
                                     <h3 class="card-title mb-1 text-dark">DATA JALAN</h3>
