1941e1e6e6c361e8f658be3547c681b3e79cd05e
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-30 12:26:33 +0700

    margin 4

--- application/views/vi_user.php
+++ application/views/vi_user.php
@@ -1,6 +1,6 @@
 <!-- CHART GRAFIK USER-->
         <div class="card-responsive">
-            <div class="card-body mx-12 px-5 my-3">
+            <div class="card-body mx-4 px-5 my-3">
             <ul style="display:grid; list-style-type:none; grid-template-columns: 50% 50%; grid-template-rows: repeat(2, auto);">
                 <li>
 		        <?php foreach ($countkasus as $value) {?>
