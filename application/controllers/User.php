8f83b921e0e1f145f7c3cbd6ea73f51631b719c8
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-30 10:27:16 +0700

    lanjut 8

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,7 +6,7 @@ class User extends CI_Controller {
 	function __construct(){
 		//parent::__construct();
         $this->CI =& get_instance();
-        $this->load->model('m_user');
+        $this->load->model('M_user');
 	}
  
 	public function index(){
