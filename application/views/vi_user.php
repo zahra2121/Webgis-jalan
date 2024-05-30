2a4812d8f30012f5f757294dcc3252c719fe5a1a
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-30 11:59:45 +0700

    margin

--- application/views/vi_user.php
+++ application/views/vi_user.php
@@ -15,7 +15,7 @@
 						</div>
                         <?php }?>
                         <?php foreach ($countblack as $key => $value) {?>
-                            <div class="col-lg-2 px-2 mx-4 py-1 grid-margin card-responsive">
+                            <div class="col-lg-2 px-2 mx-4 py-1 card-responsive">
 							<div class="card" style="background: rgba(0, 99, 132, 0.1)">
 								<div class="card-body">
                                     <h3 class="card-title mb-1 text-dark">DATA JALAN</h3><br>
@@ -27,7 +27,7 @@
 						</div>
                         <?php }?>
                         <?php foreach ($countrawan as $value) {?>
-                        <div class="col-lg-2 px-2 mx-4 py-1 grid-margin stretch-card">
+                        <div class="col-lg-2 px-2 mx-4 py-1 card-responsive">
 							<div class="card" style="background: rgba(0, 99, 132, 0.1)">
 								<div class="card-body">
                                     <h3 class="card-title mb-1 text-dark">STATUS DAERAH RAWAN KECELAKAAN</h3>
@@ -39,7 +39,7 @@
 						</div>
                         <?php }?>
                         <?php foreach ($countaman as $key => $value) {?>
-                        <div class="col-lg-2 px-1 mx-4 py-1 grid-margin stretch-card">
+                        <div class="col-lg-2 px-1 mx-4 py-1 grid-margin card-responsive">
 							<div class="card" style="background: rgba(0, 99, 132, 0.1)">
 								<div class="card-body">
                                     <h3 class="card-title mb-1 text-dark">STATUS BUKAN DAERAH RAWAN KECELAKAAN</h3>
