71b9c1b32801268a9f836ea6d4043c598335d3ad
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-31 14:50:26 +0700

    geolayer2

--- application/views/vi_user.php
+++ application/views/vi_user.php
@@ -79,7 +79,7 @@
                     <br><br>
 
                     <div id="map" style="width: 100%; height: 680px;"></div>
-                    <script src="kecamatan/bambanglipuro.js"></script>
+                    <script src="<?= base_url()?>kecamatan/bambanglipuro.js"></script>
                     <script>
                         var peta1 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFyZGFsaXVzIiwiYSI6ImNsZnVtbDdtZzAyYjMzdXRhdDN6djY5cWoifQ.Xqtyqa7hvGhQla2oAwpG_Q', {
                             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
