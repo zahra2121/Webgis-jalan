0cc105fc1b8a5749bfa037e01cae070102b7e6ee
zahra2121 <hikmatuzzahra21@gmail.com>
2024-05-31 18:15:58 +0700

    add polygon

--- application/views/vi_user.php
+++ application/views/vi_user.php
@@ -170,7 +170,7 @@
                         var map = L.map('map', {
                             center: [-7.847007587128045, 110.35543035070813],
                             zoom: 12,
-                            layers: [peta2],
+                            layers: [peta5],
                         });
 
                         var baseLayers = {
