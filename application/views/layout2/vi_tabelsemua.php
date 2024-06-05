2282f232cb84b16af7618aa9679132c01b2a8bf1
zahra2121 <hikmatuzzahra21@gmail.com>
2024-06-05 15:04:55 +0700

    add

--- application/views/layout2/vi_tabelsemua.php
+++ application/views/layout2/vi_tabelsemua.php
@@ -5,7 +5,7 @@
                 <div class="card-body">
 
                 <!-- GRAFIK ADMIN -->
-              <center><section class="col-lg-6 px-4 center grid-margin stretch-card" style="grid-template-columns: 50% 50%; grid-template-rows: repeat(2, auto);">
+              <center><section class="col-lg-6 px-4 center grid-margin stretch-card" style="grid-template-columns: 70% 30%; grid-template-rows: repeat(2, auto);">
                   <div class="card mx-2 px-4 center grid-margin stretch-card">
                       <div class="card-body"> 
                       <center><h2 class="card-title mb-2 text-dark center">GRAFIK STATUS JALAN BERDASARKAN TAHUN TERJADI KECELAKAAN LALU LINTAS</h2></center><br>
@@ -41,10 +41,10 @@
 
                             var dataFirst = {
                               label: "Status Daerah Rawan ",
-                              borderColor: 'rgba(0,0,255,1)',
+                              borderColor: 'rgb(255,0,0)',
                               backgroundColor: 'transparent',
-                              pointBorderColor: 'blue',
-                              pointBackgroundColor: 'rgba(0,0,255,1)',
+                              pointBorderColor: 'red',
+                              pointBackgroundColor: 'rgb(255,0,0))',
                               pointRadius: 5,
                               pointHoverRadius: 10,
                               pointHitRadius: 30,
