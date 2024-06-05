a6016b2244c9b65532727b03a8038f7707ec5e3b
zahra2121 <hikmatuzzahra21@gmail.com>
2024-06-05 14:34:23 +0700

    add

--- application/views/vi_user.php
+++ application/views/vi_user.php
@@ -502,10 +502,10 @@
                                     ?>
                             })
                             .bindPopup("<h5><b> Daerah Jalan<br><br><?= $value->daerah_jalan?></b><br><br> Kecamatan : <?= $value->kecamatan?><br> Tahun : <?= $value->tahun?><br> Jumlah Kasus : <?= $value->total_idkasus?><br><br> Status Jalan : <?php
-                                    if($value->status == '0' and $value->aek > $value->ucl){
+                                    if($value->status == '0' and $value->aek > $value->bca){
                                         echo "<label class='badge bg-danger text-white' name='$value->status' id='$value->status'>DAERAH RAWAN</label>";
                                     }
-                                    elseif($value->status == '1' and $value->aek < $value->ucl){
+                                    elseif($value->status == '1' and $value->aek < $value->bca){
                                         echo "<label class='badge bg-warning text-dark' name='$value->status' id='$value->status'>BUKAN DAERAH RAWAN</label>";
                                     }else{
                                         echo "<label class='badge bg-success text-white' name='$value->status' id='$value->status'>PROSES DATA</label>";
