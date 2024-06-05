6b5abd9044d77138a9e76dcfd4f3aba3ba31b4f0
zahra2121 <hikmatuzzahra21@gmail.com>
2024-06-05 14:56:57 +0700

    add

--- application/views/layout2/vi_tabelsemua.php
+++ application/views/layout2/vi_tabelsemua.php
@@ -3,6 +3,103 @@
             <div class="col-lg-12 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
+
+                <!-- GRAFIK ADMIN -->
+              <section class="col-lg-6 px-4 center grid-margin stretch-card" style="grid-template-columns: 50% 50%; grid-template-rows: repeat(2, auto);">
+                  <div class="card mx-2 px-4 center grid-margin stretch-card">
+                      <div class="card-body"> 
+                      <center><h2 class="card-title mb-2 text-dark center">GRAFIK NILAI AEK, BCA, DAN UCL BERDASARKAN DATA BLACKSPOT DALAM PENENTUAN DAERAH RAWAN KECELAKAAN LALU LINTAS</h2></center><br>
+                        
+                        <div style="width: 100%;"><canvas id="myChart1"></canvas>
+                        <?php
+                        //Inisialisasi nilai variabel awal
+                        $nama_status= "";
+                        $jum=null;
+                        $jum2=null;
+                        $jum3=null;
+                       
+                        foreach ($jumrawan as $item){
+                            $jur=$item->idblack;
+                            $nama_status .= "'$jur'". ", ";
+                            // status rawan
+                            $jum_rawan= $item->tot_rawan;
+                            $jum .= "$jum_rawan". ", ";
+                        }
+                        foreach ($jumaman as $item){
+                            // status aman
+                            $jum_aman= $item->tot_aman;
+                            $jum2 .= "$jum_aman". ", ";
+                        }
+                        foreach ($jumproses as $item){
+                          // status proses
+                          $jum_proses= $item->tot_proses;
+                          $jum3 .= "$jum_proses". ", ";
+                      }
+                        ?>
+                        <script>
+                            var ctx = document.getElementById('myChart1').getContext('2d');
+
+                            var dataFirst = {
+                              label: "Status Daerah Rawan ",
+                              borderColor: 'rgba(0,0,255,1)',
+                              backgroundColor: 'transparent',
+                              pointBorderColor: 'blue',
+                              pointBackgroundColor: 'rgba(0,0,255,1)',
+                              pointRadius: 5,
+                              pointHoverRadius: 10,
+                              pointHitRadius: 30,
+                              pointBorderWidth: 2,
+                              pointStyle: 'rectRounded',
+                              data: [<?php echo $jum; ?>],
+                              lineTension: 0.3,
+                              // Set More Options 
+                            };
+                              
+                            var dataSecond = {
+                              label: "Status Bukan Daerah Rawan ",
+                              borderColor: ['rgb(255,165,0)'],
+                              backgroundColor: 'transparent',
+                              pointBorderColor: 'orange',
+                              pointBackgroundColor: 'rgb(255,165,0)',
+                              pointRadius: 5,
+                              pointHoverRadius: 10,
+                              pointHitRadius: 30,
+                              pointBorderWidth: 2,
+                              pointStyle: 'rectRounded',
+                              data: [<?php echo $jum2; ?>],
+                              // Set More Options 
+                            };
+
+                            var dataThird = {
+                              label: "Status Proses Data ",
+                              borderColor: 'rgb(0,128,0)',
+                              backgroundColor: 'transparent',
+                              pointBorderColor: 'green',
+                              pointBackgroundColor: 'rgb(0,128,0)',
+                              pointRadius: 5,
+                              pointHoverRadius: 10,
+                              pointHitRadius: 30,
+                              pointBorderWidth: 2,
+                              pointStyle: 'rectRounded',
+                              data: [<?php echo $jum3; ?>],
+                              // Set More Options 
+                            };
+                              
+                            var speedData = {
+                              labels: [<?php echo $nama_status; ?>],
+                              datasets: [dataFirst, dataSecond, dataThird]
+                            };
+                            var lineChart = new Chart(ctx, {
+                              type: 'line',
+                              data: speedData
+                            });
+
+                        </script>
+                          </div>
+                      </div>
+                  </div>
+            </section>
+
                 <div class="card-body">
                   <a href="<?= base_url() ?>index.php/laporanfpdf">
                   <button type="button" class="btn btn-dark btn-icon-text float-right">
