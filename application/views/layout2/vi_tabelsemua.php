62aee59996353e8aaed8d85819327a440b348f03
zahra2121 <hikmatuzzahra21@gmail.com>
2024-06-05 15:09:10 +0700

    add

--- application/views/layout2/vi_tabelsemua.php
+++ application/views/layout2/vi_tabelsemua.php
@@ -5,8 +5,8 @@
                 <div class="card-body">
 
                 <!-- GRAFIK ADMIN -->
-              <center><section class="col-lg-6 px-4 center grid-margin stretch-card" style="width: 70%;">
-                  <div class="card mx-2 px-4 center grid-margin stretch-card">
+              <center><section class="col-lg-6 px-4 center grid-margin card-responsive" style="width: 70%;">
+                  <div class="card mx-2 px-4 center grid-margin">
                       <div class="card-body"> 
                       <center><h2 class="card-title mb-2 text-dark center">GRAFIK STATUS JALAN BERDASARKAN TAHUN TERJADI KECELAKAAN LALU LINTAS</h2></center><br>
                         
