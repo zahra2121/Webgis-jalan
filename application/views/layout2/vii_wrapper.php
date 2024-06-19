432e5ee9cd9cf6a0c63a8a339115eda56b6a1325
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-06-19 06:44:17 +0000

    add footer

--- application/views/layout2/vii_wrapper.php
+++ application/views/layout2/vii_wrapper.php
@@ -3,6 +3,6 @@ require_once('vi_head.php');
 require_once('vi_header.php');
 require_once('vi_nav.php');
 require_once('vi_content.php');
-require_once('v_footer.php');
+require_once('vii_footer.php');
 
 ?>
