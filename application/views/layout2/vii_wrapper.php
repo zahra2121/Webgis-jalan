a6a9fc0f6f2641340d7c6f1113bc9b92d435450f
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-06-23 12:35:19 +0000

    up header

--- application/views/layout2/vii_wrapper.php
+++ application/views/layout2/vii_wrapper.php
@@ -1,6 +1,6 @@
 <?php
 require_once('vi_head.php');
-require_once('vi_header.php');
+require_once('vii_header.php');
 require_once('vi_nav.php');
 require_once('vi_content.php');
 require_once('vii_footer.php');
