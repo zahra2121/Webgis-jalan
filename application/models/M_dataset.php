105939714c2c8a36f68d346e2899cd429f8f576f
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-06-25 07:27:04 +0000

    up

--- application/models/M_dataset.php
+++ application/models/M_dataset.php
@@ -87,8 +87,8 @@ class M_dataset extends CI_model
         $this->db->select('blackspot.*, kasus.*, blackspot.kecamatan, COUNT(blackspot.status) as tot_rawan');
         $this->db->from('blackspot');
         $this->db->join('kasus', 'blackspot.idblack = kasus.id');
-        $this->db->where('blackspot.status = "0"');
         $this->db->group_by('blackspot.kecamatan');
+        $this->db->where('blackspot.status = "0"');
         $query=$this->db->get();
         return $query->result(); 
     }
@@ -97,8 +97,8 @@ class M_dataset extends CI_model
         $this->db->select('blackspot.*, kasus.*, COUNT(blackspot.status) as tot_aman, SUM(blackspot.aek) as totalsemua_aek, SUM(blackspot.ucl) as totalsemua_ucl');
         $this->db->from('blackspot');
         $this->db->join('kasus', 'blackspot.idblack = kasus.id');
-        $this->db->where('blackspot.status = "1"');
         $this->db->group_by('blackspot.kecamatan');
+        $this->db->where('blackspot.status = "1"');
         $query=$this->db->get();
         return $query->result(); 
     }
@@ -107,8 +107,8 @@ class M_dataset extends CI_model
         $this->db->select('blackspot.*, kasus.*, COUNT(blackspot.status) as tot_proses');
         $this->db->from('blackspot');
         $this->db->join('kasus', 'blackspot.idblack = kasus.id');
-        $this->db->where('blackspot.status = "2"');
         $this->db->group_by('blackspot.kecamatan');
+        $this->db->where('blackspot.status = "2"');
         $query=$this->db->get();
         return $query->result(); 
     }
