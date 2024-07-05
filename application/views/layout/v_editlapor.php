e4eb7b8a9c0f175a4b9816de995cc6836a5dc346
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-05 11:06:41 +0000

    add

--- application/views/layout/v_editlapor.php
+++ application/views/layout/v_editlapor.php
@@ -117,9 +117,8 @@
           </table>
 
           <br>
-            <a href="<?= base_url() ?>index.php/home/lapor"><button type="button" class="btn btn-warning btn-rounded btn-fw">Back</button></a>
-            <a><button type="submit" class="btn btn-primary btn-rounded btn-fw text-white">Verifikasi</button></a>
-
+          <a href="<?= base_url('index.php/home/lapor')?>"><button type="button" class="btn btn-warning btn-rounded btn-fw">Back</button></a>
+            <button type="submit" class="btn btn-primary btn-rounded btn-fw">Verifikasi</button>
           </form>
         </div>
       </div>
