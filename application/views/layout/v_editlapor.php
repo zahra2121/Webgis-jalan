106b4a6e0b6713028e8305e734ae1505b0925503
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-05 11:36:18 +0000

    add

--- application/views/layout/v_editlapor.php
+++ application/views/layout/v_editlapor.php
@@ -1,4 +1,4 @@
-<<?php echo form_open('home/editlapor/' . $detaillapor->idlapor)?>
+<<?php echo form_open('home/editlapor/' . $editlapor->idlapor)?>
 
 <div class="main-panel">
     <br><div class="container col-10 grid-margin">
@@ -22,17 +22,17 @@
                   <div class="col-sm-4">
                     <select class="form-control" name="status_lapor">
                       <option value="" >- Pilih -</option>
-                      <option value="0" <?= $detaillapor->status_lapor == 0 ? 'selected' : ''?>>DITOLAK</option>
-                      <option value="1" <?= $detaillapor->status_lapor == 1 ? 'selected' : ''?>>DITERIMA</option>
-                      <option value="2" <?= $detaillapor->status_lapor == 2 ? 'selected' : ''?>>DIPROSES</option>
+                      <option value="0" <?= $editlapor->status_lapor == 0 ? 'selected' : ''?>>DITOLAK</option>
+                      <option value="1" <?= $editlapor->status_lapor == 1 ? 'selected' : ''?>>DITERIMA</option>
+                      <option value="2" <?= $editlapor->status_lapor == 2 ? 'selected' : ''?>>DIPROSES</option>
                     </select>
                   </div>
                 </div>
               </div>
             </div>
 
-          <p class="card-description"><b>Tanggal Lapor : </b><?Php echo date('d-m-Y', strtotime($detaillapor->tanggal_isi)) ?></p>
-          <p class="card-description"><b>Jam Lapor : </b><?php echo date('H:i', strtotime($detaillapor->tanggal_isi)) ?></p><br>
+          <p class="card-description"><b>Tanggal Lapor : </b><?Php echo date('d-m-Y', strtotime($editlapor->tanggal_isi)) ?></p>
+          <p class="card-description"><b>Jam Lapor : </b><?php echo date('H:i', strtotime($editlapor->tanggal_isi)) ?></p><br>
           <table class="table" id="dataTable" width="100%" cellspacing="0">
               <thead class="bg-success text-white">
                 <tr>
@@ -47,11 +47,11 @@
               </thead>
               <tbody>
                 <tr>
-                  <Td><?Php echo $detaillapor->alamat ?></Td>
+                  <Td><?Php echo $editlapor->alamat ?></Td>
                   <Td>
                       <?php
-                      echo date('d ', strtotime($detaillapor->tgl_kejadian));
-                      $month = date('F', strtotime($detaillapor->tgl_kejadian));
+                      echo date('d ', strtotime($editlapor->tgl_kejadian));
+                      $month = date('F', strtotime($editlapor->tgl_kejadian));
                         switch ($month) {
                           case 'January':
                             echo "Januari ";
@@ -89,21 +89,21 @@
                             default:
                               echo "Desember ";
                           }
-                          echo date(' Y', strtotime($detaillapor->tgl_kejadian));  
+                          echo date(' Y', strtotime($editlapor->tgl_kejadian));  
                           ?>
                   </Td>
-                  <Td><?Php echo date('H:i', strtotime($detaillapor->jam)) ?></Td>
-                  <Td><?Php echo "LR : ". $detaillapor->luka_ringan. "<br>";
-                    echo "LB : ". $detaillapor->luka_berat. "<br>";
-                    echo "M  : " . $detaillapor->meninggal. "<br>" ;
-                    echo "R  : " . $detaillapor->rugi. "<br>" ;
+                  <Td><?Php echo date('H:i', strtotime($editlapor->jam)) ?></Td>
+                  <Td><?Php echo "LR : ". $editlapor->luka_ringan. "<br>";
+                    echo "LB : ". $editlapor->luka_berat. "<br>";
+                    echo "M  : " . $editlapor->meninggal. "<br>" ;
+                    echo "R  : " . $editlapor->rugi. "<br>" ;
                   ?></Td>
                   <Td>
-                    <a href="<?Php echo $detaillapor->link_maps ?>"><?Php echo $detaillapor->link_maps ?></a>
+                    <a href="<?Php echo $editlapor->link_maps ?>"><?Php echo $editlapor->link_maps ?></a>
                   </Td>
                   <Td>
                     <?php
-                      $image = $detaillapor->foto;
+                      $image = $editlapor->foto;
                       if($image == null){
                           echo $img = "No Photo";
                       } else {
@@ -111,7 +111,7 @@
                       <img src='<?=base_url()?>assets/user_lapor/<?=$image;?>' style='width:130px; height:130px; border-radius: 5px; -moz-border-radius: 20px;' class="zoomable">
                     <?php } ?>
                   </Td>
-                  <Td><?Php echo $detaillapor->nama ?></Td>
+                  <Td><?Php echo $editlapor->nama ?></Td>
                 </tr>
               </tbody>
           </table>
