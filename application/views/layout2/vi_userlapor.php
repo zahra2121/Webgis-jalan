8d80394b27b3c8a408e3f239a498ea4968cf35fb
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-06 12:44:01 +0000

    data peta lapor

--- application/views/layout2/vi_userlapor.php
+++ application/views/layout2/vi_userlapor.php
@@ -108,58 +108,9 @@
                         <div class="form-group row">
                           <label class="col-sm-3 col-form-label">Titik Lokasi Kejadian</label>
                           <div class="col-sm-9">
-                            <script src="assets/js/geo.js" type="text/javascript" charset="utf-8"></script>
-                            <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-
-                            <script>
-                                function initialize_map()
-                                {
-                                    var myOptions = {
-                                        zoom: 4,
-                                        mapTypeControl: true,
-                                        mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
-                                        navigationControl: true,
-                                        navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
-                                        mapTypeId: google.maps.MapTypeId.ROADMAP      
-                                      }	
-                                  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-                                }
-                                function initialize()
-                                {
-                                  if(geo_position_js.init())
-                                  {
-                                    document.getElementById('current').innerHTML="Receiving...";
-                                    geo_position_js.getCurrentPosition(show_position,function(){document.getElementById('current').innerHTML="Couldn't get location"},{enableHighAccuracy:true});
-                                  }
-                                  else
-                                  {
-                                    document.getElementById('current').innerHTML="Functionality not available";
-                                  }
-                                }
-
-                                function show_position(p)
-                                {
-                                  document.getElementById('current').innerHTML="latitude="+p.coords.latitude.toFixed(2)+" longitude="+p.coords.longitude.toFixed(2);
-                                  var pos=new google.maps.LatLng(p.coords.latitude,p.coords.longitude);
-                                  map.setCenter(pos);
-                                  map.setZoom(14);
-
-                                  var infowindow = new google.maps.InfoWindow({
-                                      content: "<strong>yes</strong>"
-                                  });
-
-                                  var marker = new google.maps.Marker({
-                                      position: pos,
-                                      map: map,
-                                      title:"You are here"
-                                  });
-
-                                  google.maps.event.addListener(marker, 'click', function() {
-                                    infowindow.open(map,marker);
-                                  });
-                                  
-                                }
-                            </script>
+                            <div id="title">Show Position In Map</div>
+                            <div id="current">Initializing...</div>
+                            <div id="map_canvas" style="width:100%; height:350px"></div>
 
                           </div>
                         </div>
