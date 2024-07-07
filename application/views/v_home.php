adc0e86b6f78385ffa2bbb122acf2fa11684e3fe
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-07 12:50:58 +0000

    add

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -210,7 +210,7 @@
                             type: 'bar',
                             data: speedData,
                             options: chartOptions,
-                            plugins: [chartOptions] // Add this line
+                            plugins: [datalabels] // Add this line
                         };
 
                         var barChart = new Chart(ctx, config);
