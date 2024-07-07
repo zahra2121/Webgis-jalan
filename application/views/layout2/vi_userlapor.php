1842a9ec8874a125279791b63ccc8419956ed408
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-07 03:54:24 +0000

    up

--- application/views/layout2/vi_userlapor.php
+++ application/views/layout2/vi_userlapor.php
@@ -109,11 +109,13 @@
                           <label class="col-sm-3 col-form-label">Titik Lokasi Kejadian</label>
                           
                           <div class="col-sm-9">
-                            <div class="col-sm-3">
-                              <input type="text" name="latitude" class="form-control" placeholder="Latitude" />
-                            </div>&nbsp;
-                            <div class="col-sm-3">
-                              <input type="text" name="longitude" class="form-control" placeholder="Longitude" />
+                            <div class="row">
+                                <div class="col-sm-3">
+                                  <input type="text" name="latitude" class="form-control" placeholder="Latitude" />
+                                </div>&nbsp;
+                                <div class="col-sm-3">
+                                  <input type="text" name="longitude" class="form-control" placeholder="Longitude" />
+                                </div>
                             </div>
                             
                             <body onload="initialize_map(); initialize()">
