ca6a57de753ea2a38fdcd81dc314bb2209a0b8c7
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-07 04:37:51 +0000

    ass

--- application/views/layout2/vi_userlapor.php
+++ application/views/layout2/vi_userlapor.php
@@ -113,6 +113,7 @@
                               <div class="row">
                                   <div class="col-sm-5">
                                     <input type="text" id="latitude" name="latitude" class="form-control" placeholder="Latitude" />
+                                    <div id="latitude"><input type="text" id="latitude" name="latitude" class="form-control" placeholder="Latitude" /></div>
                                   </div>&nbsp;
                                   <div class="col-sm-5">
                                     <input type="text" id="longitude" name="longitude" class="form-control" placeholder="Longitude" />
