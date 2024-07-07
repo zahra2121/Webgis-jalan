2252fa4612fc284670e8bfd95b360bad58e3cc69
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-07 12:12:33 +0000

    add

--- application/controllers/Laporfpdf.php
+++ application/controllers/Laporfpdf.php
@@ -37,7 +37,7 @@ class Laporfpdf extends CI_Controller {
         $pdf->Cell(60,7,'DOKUMENTASI',1,0,'C');
         
         $pdf->Cell(10,7,'',0,1);
-        $pdf->SetFont('Arial','',8);
+        $pdf->SetFont('Arial','',9);
         $lapor = $this->db->get('lapor')->result();
         $no=0;
     
@@ -45,7 +45,7 @@ class Laporfpdf extends CI_Controller {
             $no++;
             //$jumlah = $data->luka_ringan + $data->luka_berat + $data->meninggal;
            
-            $pdf->Cell(10,6,$no,1,0, 'C');
+            $pdf->Cell(10,6,$no,1,1, 'C');
             $pdf->Cell(40,6,$data->tanggal_isi,1,0);
             $pdf->Cell(170,6,$data->alamat,1,0);
             $pdf->Cell(20,6,$data->tgl_kejadian,1,0);
