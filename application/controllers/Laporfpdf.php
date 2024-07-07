b302a3ccf206e301c165dedde0e5300523538019
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-07 12:06:50 +0000

    add

--- application/controllers/Laporfpdf.php
+++ application/controllers/Laporfpdf.php
@@ -51,7 +51,7 @@ class Laporfpdf extends CI_Controller {
             $pdf->Cell(20,7,$data->tgl_kejadian,1,0);
             $pdf->Cell(20,7,$data->jam,1,0,'C');
 
-            $pdf->MultiCell(80,6,$data->link_maps . "\nLat: " . $data->latitude . ", Long: " . $data->longitude,1);
+            $pdf->MultiCell(80,7,$data->link_maps . "\nLat: " . $data->latitude . ", Long: " . $data->longitude,1);
     
             // Adjust the next cell's position since MultiCell moves to the next line
             $pdf->SetXY($pdf->GetX() + 340, $pdf->GetY() - 6); // Adjust X and Y as needed
