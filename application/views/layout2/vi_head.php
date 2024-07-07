865bb930e60078b8e5fbff63a124120430c3ac18
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-07 03:08:29 +0000

    update

--- application/views/layout2/vi_head.php
+++ application/views/layout2/vi_head.php
@@ -82,7 +82,7 @@
             }
 
             function getAddress(lat, lng) {
-                var geocoder = new google.maps.geo_position_js();
+                var geocoder = new google.maps.getCurrentPosition();
                 var latlng = new google.maps.LatLng(lat, lng);
                 geocoder.geocode({ 'location': latlng }, function(results, status) {
                     if (status === 'OK') {
