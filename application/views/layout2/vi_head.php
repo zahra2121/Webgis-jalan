3f8d1db695523aa4f62af276512d83744a920e9d
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-08 04:06:35 +0000

    add

--- application/views/layout2/vi_head.php
+++ application/views/layout2/vi_head.php
@@ -84,7 +84,33 @@
 
                 google.maps.event.addListener(marker, 'click', function() {
                 infowindow.open(map,marker);
-                });    
+                });
+                
+                $latitude 	= p.coords.latitude;
+                $longitude	= p.coords.longitude;
+
+                if (!empty($latitude) && !empty($longitude)) {
+
+                    $gmap = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($latitude).','.$longitude.'&sensor=false';
+                    // curl
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_URL, $gmap);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($ch, CURLOPT_PROXYPORT,3128);
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,0);
+                    $response = curl_exec($ch);
+                    curl_close($ch);
+                    // end curl
+                    $data = json_decode($response);
+
+                    if ($response) {
+                        echo json_encode($data->results[0]->formatted_address);
+                    }else{
+                        echo json_encode(false);
+                    }
+                }
             }
 
             function getAddress(lat, lng) {
@@ -105,32 +131,6 @@
                             "<br>Geocoder failed due to: " + status;
                     }
                 });
-            }
-
-            $latitude 	= $_POST['latitude'];
-            $longitude	= $_POST['longitude'];
-
-            if (!empty($latitude) && !empty($longitude)) {
-
-                $gmap = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($latitude).','.$longitude.'&sensor=false';
-                // curl
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, $gmap);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_PROXYPORT,3128);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,0);
-                $response = curl_exec($ch);
-                curl_close($ch);
-                // end curl
-                $data = json_decode($response);
-
-                if ($response) {
-                    echo json_encode($data->results[0]->formatted_address);
-                }else{
-                    echo json_encode(false);
-                }
             }
  
         </script>
