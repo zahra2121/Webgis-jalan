bdf8242e9713a606c2a54de794ba6ab4a7e6b324
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-08 01:55:31 +0000

    add

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -656,7 +656,7 @@
                                         }
                                     ?>
                             })
-                            .bindPopup("<h5><b> Daerah Jalan <?= $value->idblack?> <br><br><?= $value->daerah_jalan?></b><br><br> Patokan Jalan :<br><?=$black->patokan?><br><br> Kasus terdahulu : <?= $value->tanggal?><br><br> Status Jalan : <?php
+                            .bindPopup("<h5><b> Daerah Jalan <?= $value->idblack?> <br><br><?= $value->patokan?></b><br><br> Patokan Jalan :<br><?=$black->patokan?><br><br> Kasus terdahulu : <?= $value->tanggal?><br><br> Status Jalan : <?php
                                     if($value->status == '0' and $value->aek > $value->bca){
                                         echo "<label class='badge bg-danger text-white' name='$value->status' id='$value->status'>DAERAH RAWAN</label>";
                                     }
