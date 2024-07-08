985399dca309bdd273f7dc9b6204717cb619a7af
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-08 12:54:03 +0000

    add

--- application/views/layout2/vi_userlapor.php
+++ application/views/layout2/vi_userlapor.php
@@ -118,7 +118,7 @@
                               <div id="lokasi">Lokasi Anda saat ini: <span id="lokasi"></span></div>
                               <div id="lokasi">Initializing...</div>
                               <div id="current">Initializing...</div>
-                              <div id="map_lokasi" style="width:300px; height:200px"></div>
+                              <div id="map_lokasi" style="width:100%; height:350px"></div>
 
 
                             </body>
