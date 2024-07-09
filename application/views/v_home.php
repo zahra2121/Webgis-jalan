265714e59eaffcca5df810203d962dcc02530626
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-09 03:28:25 +0000

    add

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -704,7 +704,7 @@
                                             // fillColor: '#FF0000',
                                             // fillOpacity: 0.7,
                                             // radius: 150";
-                                            'icon: redIcon';
+                                            "icon: redIcon";
                                         }
                                         elseif($value->status == '1' and $value->aek < $value->bca){
                                             // echo "color: 'yellow',
@@ -712,7 +712,7 @@
                                             // fillOpacity: 0.3,
                                             // borderOpacity: 0.5,
                                             // radius: 150";
-                                            'icon: yellowIcon';
+                                            "icon: yellowIcon";
                                         }
                                         elseif($value->status == '2' and $value->aek == 0){
                                             echo "color: 'green',
