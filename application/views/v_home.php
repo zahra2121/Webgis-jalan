7833aa925f4adc894f3b05d92e612b191b025576
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-09 03:26:19 +0000

    add

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -351,8 +351,17 @@
                             attribution: 'Map data &copy; <a href="https://www.google.com/maps">Google Maps</a>'
                         });
 
-                        var yellowIcon = new LeafIcon({iconUrl: '<?= base_url()?>/assets/marker2.png'}),
-                            redIcon = new LeafIcon({iconUrl: '<?= base_url()?>/assets/marker1.png'});
+                        // Definisi custom icon
+                        var LeafIcon = L.Icon.extend({
+                            options: {
+                                iconSize:     [38, 95],
+                                iconAnchor:   [22, 94],
+                                popupAnchor:  [-3, -76]
+                            }
+                        });
+
+                        var yellowIcon = new LeafIcon({iconUrl: '<?= base_url()?>assets/marker2.png'}),
+                            redIcon = new LeafIcon({iconUrl: '<?= base_url()?>assets/marker1.png'});
 
                         var map = L.map('map', {
                             center: [-7.889229799481091, 110.34618188086941],
