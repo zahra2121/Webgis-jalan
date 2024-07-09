a4c751124e15aa5191db8ec9708db33c65e76b21
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-09 05:56:03 +0000

    add

--- application/models/M_dataset.php
+++ application/models/M_dataset.php
@@ -20,7 +20,7 @@ class M_dataset extends CI_model
     }
 
     public function all_black(){
-        $this->db->select('blackspot.*, kasus.*, kasus.tanggal, blackspot.patokan as patok_black, blackspot.tahun as tahun_black, SUM(blackspot.aek) as totalsemua_aek, COUNT(blackspot.idblack) as total_data, COUNT(kasus.id) as total_idkasus, COUNT(blackspot.aek) as total_kasus, SUM(kasus.luka_ringan) as lr_aek, SUM(kasus.luka_berat) as lb_aek, SUM(kasus.meninggal) as m_aek, SUM(kasus.rugi) as r_aek');
+        $this->db->select('blackspot.*, kasus.*, kasus.tanggal, COUNT(blackspot.patokan) as patok_black, blackspot.tahun as tahun_black, SUM(blackspot.aek) as totalsemua_aek, COUNT(blackspot.idblack) as total_data, COUNT(kasus.id) as total_idkasus, COUNT(blackspot.aek) as total_kasus, SUM(kasus.luka_ringan) as lr_aek, SUM(kasus.luka_berat) as lb_aek, SUM(kasus.meninggal) as m_aek, SUM(kasus.rugi) as r_aek');
         $this->db->from('blackspot');
         $this->db->join('kasus', 'blackspot.idblack = kasus.id', 'left');
         $this->db->group_by('kasus.id');
