a7bc1036444255ca3a89c4ee19b2f18afb3dcba4
hikmatuzzahra <87288056+zahra2121@users.noreply.github.com>
2024-07-10 02:00:28 +0000

    add

--- application/models/M_dataset.php
+++ application/models/M_dataset.php
@@ -32,7 +32,7 @@ class M_dataset extends CI_model
         $this->db->select('blackspot.*, kasus.*, kasus.tanggal, blackspot.patokan as patok, COUNT(blackspot.idblack) as total_data, COUNT(kasus.id) as total_idkasus');
         $this->db->from('blackspot');
         $this->db->join('kasus', 'blackspot.idblack = kasus.id', 'left');
-        $this->db->group_by('blackspot.idblack');
+        $this->db->group_by('blackspot.patokan');
         $query=$this->db->get();
         return $query->result(); 
     }
